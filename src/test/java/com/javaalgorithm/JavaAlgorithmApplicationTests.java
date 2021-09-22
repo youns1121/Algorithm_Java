b260bcd98373481719df17544eee25806ac4a3d7
youns <minys94@daum.net>
2021-09-23 00:31:09 +0900

    자바_알고리즘 세팅 완료

---
+++ src/test/java/com/javaalgorithm/JavaAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
